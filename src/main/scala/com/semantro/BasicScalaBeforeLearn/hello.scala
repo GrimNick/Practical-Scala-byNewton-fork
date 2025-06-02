185708bb773c782114d88b765e20c3ee6e4632a4
Newton Shahi Thakuri <114736550+GrimNick@users.noreply.github.com>
2025-06-02 08:30:12 +0545

    added Basics of Scala before Learn

---
+++ src/main/scala/com/semantro/BasicScalaBeforeLearn/hello.scala
@@ -0,0 +1,9 @@
+object hello{
+  def main(args: Array[String]): Unit ={
+    println("Hello, Scala 2!")
+  }
+}
+
+//object = singleton object (no static keyword in scala!)
+//Unit is like void in java
+
