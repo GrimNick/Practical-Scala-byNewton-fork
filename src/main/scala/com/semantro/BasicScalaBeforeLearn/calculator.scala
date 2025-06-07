20149c96810a6fe0fc478a2d5abd36d14cdb9436
Newton Shahi Thakuri <114736550+GrimNick@users.noreply.github.com>
2025-06-07 20:34:20 +0545

    added calculator

--- src/main/scala/com/semantro/BasicScalaBeforeLearn/calculator.scala
+++ src/main/scala/com/semantro/BasicScalaBeforeLearn/calculator.scala
@@ -1,53 +1,18 @@
-//Simply another implementation of higher order function
+import scala.io.StdIn.{readChar, readInt}
 
-object calculator{
-  private def add(a: Int, b: Int): Int = a+b
-  private def sub(a: Int, b: Int): Int = a-b
-  private def mul(a: Int, b: Int): Int = a*b
-  private def div(a: Int, b: Int): Double = a.toDouble/b
+object calculator extends App {
+  println("Give two integer")
+  val a = readInt()
+  val b = readInt()
+  println("Give an operator")
+  val op = readChar()
 
-  // (Int,Int) defines its arguments
-  // Any defines its return type which can be any datatypes
-
-  private def operator(op: String): (Int,Int) => Any = {
-    op match{
-      case "+" => add
-      case "-" => sub
-      case "*" => mul
-      case "/" => div
-      case _ => (_,_) => "Eror with input"
-    }
+  val result = op match{
+    case '+' => a+b
+    case '-' => a-b
+    case '*' => a*b
+    case '/' => a/b
+    case _ => "nothing"
   }
-
-  def main(args : Array[String]): Unit = {
-    val adder = operator("+")
-    val subber = operator("-")
-    val muller = operator("*")
-    val divider = operator("/")
-    println("Sum of 2 and 3 is " + adder(2,3) +"\nSub of 3 and 2 is "+subber(3,2)+
-      "\nMultiplication of 2 and 3 is "+muller(2,3) +"\nDivision of 4 and 3 is "+divider(4,3))
-
-  }
+  println("ans is : "+ result)
 }
-
-//Another way to solution. Simply practice
-
-//object calculator{
-//  def add1(x: Int, y: Int): Int = x+y
-//  def subtract1(x:Int, y:Int): Int = x-y
-//  def multiply1(x: Int,y: Int): Int = x*y
-//  def divide(x:Int, y:Int): Double = x.toDouble/y
-//  def operate(op: String): (Int, Int) => Any = {
-//    op match {
-//      case "add"  => add1
-//      case "subtract" =>subtract1
-//      case "multiply" => multiply1
-//      case "divide"   => divide
-//      case _  => (_,_)  => "Invalid operation"
-//    }
-//  }
-//  def main(args: Array[String]): Unit ={
-//    val divider = operate("divide")
-//    println("The division is " + divider(4,2))
-//  }
-//}
